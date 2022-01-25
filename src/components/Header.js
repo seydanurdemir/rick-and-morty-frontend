be8beba3eec7c7eb071a473cfaadc2b28f0f958f
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2022-01-26 00:19:52 +0300

    edited header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,19 @@ import './Header.css';
 const Header = () => {
     return (
         <div className="header">
-            <p className="text">Rick and Morty</p>
-            <div className="icon">
-                <img
-                    alt=""
-                    className="vector"
-                    src="https://static.overlay-tech.com/assets/a871ad99-75e9-4cc9-ac37-a6ef2fb293df.svg"
-                />
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-sm-12 col-md-6 d-flex justify-content-around'>
+                        <div className='menu'>
+                            <p className="text">Rick and Morty</p>
+                            <img
+                                alt=""
+                                className="icon"
+                                src="https://static.overlay-tech.com/assets/a871ad99-75e9-4cc9-ac37-a6ef2fb293df.svg"
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
